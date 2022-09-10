57b667f5f90ffca24c6021eff2066c0164df6166
Bessa Jonathan <hauskwar@gmail.com>
2022-09-10 12:27:15 +0200

    Commit simple

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import AddToCart from './components/AddToCart.vue';
+import NavbarCart from './components/NavbarCart.vue';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+const app = createApp();
+
+app.component('AddToCart', AddToCart);
+app.component('NavbarCart', NavbarCart);
+
+app.mount('#app');
